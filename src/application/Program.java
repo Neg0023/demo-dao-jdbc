5d6bb727d31f687333f5e6160e9565d75989a6e3
Uemerson Santos <uemerson161@hotmail.com>
2022-06-24 18:07:17 -0300

    Implementação delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -39,6 +41,13 @@ public class Program {
         seller.setName("Marta Wayne");
         sellerDao.update(seller);
         System.out.println("Update Completed");
+
+        System.out.println("\n====TEST 6: seller delete ====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        sellerDao.deleteById(10);
+        System.out.println("Delete completed");
 
     }
 }
