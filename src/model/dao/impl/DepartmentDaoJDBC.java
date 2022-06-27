17dac697803b6536fbcc13dd8946aa3f3c43be26
Uemerson Santos <uemerson161@hotmail.com>
2022-06-27 15:52:00 -0300

    Classe Program2 criada e findById implementado

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,9 +1,14 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -31,7 +36,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = connection.prepareStatement("SELECT department.* FROM department WHERE department.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                return instantiateDepartment(rs);
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department department = new Department();
+        department.setId(rs.getInt("Id"));
+        department.setName(rs.getString("Name"));
+        return department;
     }
 
     @Override
