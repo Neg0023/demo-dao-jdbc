6ff07e8adbd27e08113ce07caae51d198713e9c9
Uemerson Santos <uemerson161@hotmail.com>
2022-07-09 15:07:04 -0300

    Metodo deleteById da classe DepartmentDaoJDBC implementado

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -3,13 +3,14 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("====TEST 1: department findById ====");
@@ -32,5 +33,11 @@ public class Program2 {
         department.setName("Sports");
         departmentDao.update(department);
         System.out.println("Update Completed");
+
+        System.out.println("\n====TEST 5: department delete ====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed");
     }
 }
